791a699ab2f1c6805f197b81b5453ba8f2ee27ef
Abduh Salam <37413887+abduhsalam@users.noreply.github.com>
2026-03-26 17:52:37 +0700

    Refactor SQL cleaning scripts for better handling

--- task2a.sql
+++ task2a.sql
@@ -1,21 +1,20 @@
--- Cleaning sales_raw
-
+-- Cleaning customers_raw
 CREATE TABLE customers_clean AS
 SELECT
   id,
   name,
   CASE
+    WHEN dob IS NULL THEN NULL
     WHEN dob = '1900-01-01' THEN NULL
-    WHEN dob LIKE '%/%/%' THEN STR_TO_DATE(dob, '%d/%m/%Y')
-    WHEN dob LIKE '%-%-%' THEN STR_TO_DATE(dob, '%Y-%m-%d')
+    WHEN dob REGEXP '^[0-9]{4}/[0-9]{2}/[0-9]{2}$' THEN STR_TO_DATE(dob, '%Y/%m/%d')
+    WHEN dob REGEXP '^[0-9]{2}/[0-9]{2}/[0-9]{4}$' THEN STR_TO_DATE(dob, '%d/%m/%Y')
+    WHEN dob REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$' THEN STR_TO_DATE(dob, '%Y-%m-%d')
     ELSE NULL
   END AS dob,
   created_at
 FROM customers_raw;
 
-
--- Cleaning after_sales_raw
-
+-- Cleaning sales_raw
 CREATE TABLE sales_clean AS
 SELECT
   vin,
@@ -26,26 +25,25 @@ SELECT
   created_at
 FROM sales_raw;
 
-
--- Cleaning customer_addresses_raw
-
+-- Cleaning after_sales_raw
 CREATE TABLE after_sales_clean AS
 SELECT a.*
 FROM after_sales_raw a
-INNER JOIN customers_clean c 
+INNER JOIN customers_clean c
   ON a.customer_id = c.id
-WHERE 
+WHERE
   a.vin IS NOT NULL
   AND a.service_date IS NOT NULL;
 
-
 -- Cleaning customer_addresses_raw
-
 CREATE TABLE customer_addresses_clean AS
 SELECT
   customer_id,
   address,
-  INITCAP(city) AS city,
-  INITCAP(province) AS province,
+  city,
+  CASE
+    WHEN UPPER(province) LIKE 'DKI%' THEN 'DKI Jakarta'
+    ELSE province
+  END AS province,
   created_at
 FROM customer_addresses_raw;
